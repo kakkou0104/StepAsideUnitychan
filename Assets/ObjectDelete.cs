8ac5a18c05edb7e35fdcf8ab9fa41156750a5efa
kakkou0104 <kakkou0104@gmail.com>
2017-09-05 22:07:26 +0900

    StepAsideUnitychan 第一回コミット
    ObjectDelete.csに基本課題、ItemGenerator2に発展課題

---
+++ Assets/ObjectDelete.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectDelete : MonoBehaviour {
+	//Unityちゃんのオブジェクト
+	private GameObject unitychan;
+	//Unityちゃんと壁の距離
+	private float distance;
+
+	// Use this for initialization
+	void Start () {
+		//Unityちゃんオブジェクトを取得
+		this.unitychan = GameObject.Find("unitychan");
+		//Unityちゃんとカメラの位置（z座標）の差を求める
+		this.distance = unitychan.transform.position.z - this.transform.position.z;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Unityちゃんの位置に合わせて壁を移動
+		this.transform.position = new Vector3 (0, this.transform.position.y,
+			this.unitychan.transform.position.z - distance);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "CarTag" || other.gameObject.tag ==
+		   "TrafficConeTag" || other.gameObject.tag == "CoinTag") {
+			Destroy (other.gameObject);
+
+		}
+	}
+		
+}
